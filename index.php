caae84b2aed05301b14bbfb5ce2bc93ff63cab71
Pavel <sanpaulo760@gmail.com>
2018-11-09 20:00:26 +0500

    add header

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MY MARKET</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css">
+</head>
+<body>
+<h1>Добро Пожаловать В MY MARKET</h1>
+<p>
+	<a href="product.php">Товары</a>
+	<form></form>
+</p>
+<script type="text/javascript" src="/bootstrap/js/query.js"></script>
+<script type="text/javascript" src="/bootstrap/js/popper.js"></script>
+<script type="text/javascript" src="/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
